48d3327341c447dd3bcf902f2d989eb3efb313a9
komandor <koletttut@gmail.com>
2026-02-04 22:55:45 +0200

    add hm9

---
+++ src/js/2-form.js
@@ -0,0 +1,34 @@
+let formData = { email: "", message: "" };
+const storageKey = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+
+const rawData = localStorage.getItem(storageKey);
+if (rawData) {
+    formData = JSON.parse(rawData);
+    form.elements.email.value = formData.email || "";
+    form.elements.message.value = formData.message || "";
+}
+
+form.addEventListener("input", (event) => {
+    const fieldName = event.target.name;
+    const fieldValue = event.target.value.trim();
+
+    formData[fieldName] = fieldValue;
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (!formData.email || !formData.message) {
+        alert("Fill please all fields");
+        return;
+    }
+
+    console.log(formData);
+    
+    localStorage.removeItem(storageKey);
+    formData = { email: "", message: "" };
+    form.reset();
+});
